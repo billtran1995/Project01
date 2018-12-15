b6a308303f6bf86f5fcc6776b723073965324702
Bill Tran <redbill1995@gmail.com>
2018-12-14 23:19:02 -0600

    Initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+// Require package
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Initialize app
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080, () => {
+  console.log("Server started");
+});
